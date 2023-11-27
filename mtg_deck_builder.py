e19af3c42fb6ac456558fc439c994dd4f75051c2
almmaasoglu <almmaasoglu@gmail.com>
2023-11-27 18:09:23 +0100

    Latest builder change

--- mtg_deck_builder.py
+++ mtg_deck_builder.py
@@ -15,32 +15,24 @@ if not openai_api_key:
 # Instantiate the OpenAI client with your API key
 client = OpenAI(api_key=openai_api_key)
 
-def get_deck_recommendation(description, meta_decks):
+def get_deck_recommendation(description):
     try:
-        # Constructing a string that includes both deck names and their card names
-        deck_info_strings = []
-        for deck in meta_decks:
-            deck_info = f"{deck['name']} (Cards: {', '.join(deck['cards'])})"
-            deck_info_strings.append(deck_info)
-
-        meta_info = '; '.join(deck_info_strings)
-        meta_prompt = f"Considering the current meta decks: {meta_info}, {description}"
+        meta_prompt = f"Suggest a deck based on: {description}"
         
         completion = client.chat.completions.create(
-                model="gpt-4-1106-preview",
-                messages=[
-                    {
-                        "role": "system",
-                        "content": (
-                            "You are an assistant knowledgeable about Magic: The Gathering. "
-                            "Suggest unique and niche decks based on player's choice of format. "
-                            "Make sure to be very thoughtful with card choices and additions. "
-                            "Decks that should be rare so it's not often played. "
-                            "Decreasing the counter potential of the cards. "
-                            "Make sure to take the current meta into account. "
-                        )
-                    },
-                    {"role": "user", "content": meta_prompt}
+            model="gpt-4-1106-preview",
+            messages=[
+                {
+                    "role": "system",
+                    "content": (
+                        "You are an assistant knowledgeable about Magic: The Gathering. "
+                        "Suggest unique and niche decks based on player's choice of format. "
+                        "Make sure to be very thoughtful with card choices and additions. "
+                        "Decks that should be rare so it's not often played. "
+                        "Decreasing the counter potential of the cards."
+                    )
+                },
+                {"role": "user", "content": meta_prompt}
             ]
         )            
         return completion.choices[0].message.content
@@ -66,6 +58,26 @@ def fetch_cards_from_mtg_api(card_names):
             }
             detailed_cards.append(card_details)
     return detailed_cards
+
+def main():
+    user_input = input("Describe your request for a Magic: The Gathering deck: ")
+    deck_recommendation = get_deck_recommendation(user_input)
+    
+    if deck_recommendation:
+        print("\nDeck Recommendation:")
+        print(deck_recommendation)
+
+        card_names = extract_card_names(deck_recommendation)
+        detailed_cards = fetch_cards_from_mtg_api(card_names)
+
+        if detailed_cards:
+            print("\nDetailed Card Information:")
+            for card in detailed_cards:
+                print(f"Name: {card['name']}, Mana Cost: {card['mana_cost']}, Type: {card['type']}, Rarity: {card['rarity']}, Text: {card['text']}")
+        else:
+            print("No detailed card information found.")
+    else:
+        print("Unable to generate deck recommendation.")
 
 if __name__ == "__main__":
     main()
